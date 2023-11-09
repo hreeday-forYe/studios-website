d5de438b696b4bf3703dde7335025a33cc01d5da
hridaya <hridayamishra225@gmail.com>
2023-11-09 17:10:20 +0545

    initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector('#menu-btn')
+const menu = document.getElementById('menu')
+
+btn.addEventListener('click', ()=>{
+  btn.classList.toggle('open')
+  menu.classList.toggle('flex')
+  menu.classList.toggle('hidden')
+})
